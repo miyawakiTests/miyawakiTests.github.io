6df76bc6bf2c1df1f1c83e641eaf90a3b914cbf1
CNKYOTO\miyawaki <miyawaki@infonic.co.jp>
2017-06-27 13:51:11 +0900

    ファイルの追加

---
+++ counter/index.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+	</head>
+	<body>
+		<p>Ç±ÇÒÇ…ÇøÇÕÅI</p>
+		<script src="https://www.gstatic.com/firebasejs/4.1.3/firebase.js"></script>
+		<script>
+		  // Initialize Firebase
+		  var config = {
+		    apiKey: "AIzaSyDTrbn8JbeBpQtXAy9qSkdG0nIfXh0ttBg",
+		    authDomain: "realtimevisitor.firebaseapp.com",
+		    databaseURL: "https://realtimevisitor.firebaseio.com",
+		    projectId: "realtimevisitor",
+		    storageBucket: "realtimevisitor.appspot.com",
+		    messagingSenderId: "357407106723"
+		  };
+		  firebase.initializeApp(config);
+		</script>
+	</body>
+</html>
